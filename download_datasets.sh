5b4144d832479b62800a41e63d311fd72676d0e6
Florian <florian.letsch@twentybn.com>
2020-04-25 12:07:28 +0200

    Remove unneeded datasets

--- download_datasets.sh
+++ download_datasets.sh
@@ -2,7 +2,5 @@
 DATASET_DIR=data
 mkdir -p $DATASET_DIR
 
-# Open street map data (one region only for now). Download size 149MB
-wget -P $DATASET_DIR http://download.openstreetmap.fr/extracts/europe/spain/galicia-latest.osm.pbf
+# Open street map data (one region only for now). Download size 75MB
 wget -P $DATASET_DIR http://download.openstreetmap.fr/extracts/europe/spain/galicia.osm.pbf
-wget -P $DATASET_DIR http://download.openstreetmap.fr/extracts/europe/spain/galicia.state.txt
